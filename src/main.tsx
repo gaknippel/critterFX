17098247c63261bd7c2e39a09f8adef0114d662b
Greyson Knippel <gaknippel@hotmail.com>
2026-03-14 16:44:45 -0700

    changed improper import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import router from './router'
-import "./App.css"
 import { RouterProvider } from 'react-router-dom'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
