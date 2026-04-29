fac89a2d2f16437278b62cd6a231d464eda6c7ba
Greyson Knippel <gaknippel@hotmail.com>
2026-04-28 22:45:36 -0700

    so we updating guys

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -279,6 +279,8 @@ fn write_binary_file(path: String, contents: Vec<u8>) -> Result<(), String> {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())  
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
